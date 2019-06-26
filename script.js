1254c8c3c2d1b15e4370397484b933fb2cba8a27
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2019-06-26 19:38:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function complexConversion (text) {
+  // Your code here
+}
+
+// TEST CASES
+console.log(complexConversion('name:Dimitri,email:dimitri@mail.com'));
+// { name: 'Dimitri', email: 'dimitri@mail.com' }
+
+console.log(complexConversion('name:Erwin,gender:male,age:21'));
+// { name: 'Erwin', gender: 'male', age: '21' }
+
+console.log(complexConversion('city:Surabaya,province:East Java'));
+// { city: 'Surabaya', province: 'East Java' }
